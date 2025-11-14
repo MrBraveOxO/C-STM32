445bb519cf21bbe411100e607b3544989b5ca133
YAO <bongbong0983@gmail.com>
2025-11-14 15:12:22 +0800

    1114review

---
+++ pra1/source/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int amount;
+    printf("請輸入數字：");
+    scanf("%d", &amount);
+    printf("\n你輸入的數字是：%d",amount);
+
+    return 0;
+}
